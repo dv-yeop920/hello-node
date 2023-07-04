81038bc874b5ae08f47a96bb03c87392faca7dff
junyeoplee <jyeop920@gmail.com>
2023-07-05 02:16:31 +0900

    [Add] : mongoDB에 객체 데이터 저장해보기

--- todo/server.js
+++ todo/server.js
@@ -7,7 +7,17 @@ const port = 8080;
 const URL = 'mongodb+srv://jyeop920:toddlf0826@cluster0.mvqy3yr.mongodb.net/?retryWrites=true&w=majority'
 app.use(bodyParser.urlencoded({extended: true}));
 
+let db;
+
+const obj = {
+    id:0,
+    name:'Lee',
+    age:27
+}
+
 MongoClient.connect(URL , (error , client) => {
+    if(error) return console.log(error);
+    db = client.db('todo');
     app.listen(port , () => {
         console.log('hello world');
     });
@@ -20,7 +30,15 @@ app.get('/todo' , (request , response) => {
 
 app.post('/add' , (request , response) => {
     response.send(request.body.content)
+    let obj = {
+        id:1,
+        content: request.body.content,
+        date:'2023-07-05'
+    }
     console.log(request.body.content)
+    db.collection('post').insertOne(obj , (error , result) => {
+        console.log('저장 성공')
+    })
 })
 
 
