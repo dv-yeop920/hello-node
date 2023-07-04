3ccca94a5e8dfb2af9d2e23b71bedf8a707d293c
junyeoplee <jyeop920@gmail.com>
2023-07-05 00:57:35 +0900

    [Add]: mongoDB 설치 및 node와 연동

--- todo/server.js
+++ todo/server.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
 const port = 8080;
-
+const URL = 'mongodb+srv://jyeop920:toddlf0826@cluster0.mvqy3yr.mongodb.net/?retryWrites=true&w=majority'
 app.use(bodyParser.urlencoded({extended: true}));
+
+MongoClient.connect(URL , (error , client) => {
+    app.listen(port , () => {
+        console.log('hello world');
+    });
+});
 
 
 app.get('/todo' , (request , response) => {
     response.sendFile(path.join(__dirname,'public','index.html'));
-});
-
-app.listen(port , () => {
-    console.log('hello world');
 });
 
 app.post('/add' , (request , response) => {
