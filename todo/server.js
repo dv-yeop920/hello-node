dd25b951d0f95f3c44159792ed50099cac679ab5
junyeoplee <jyeop920@gmail.com>
2023-07-05 17:51:31 +0900

    [Add] : 글등록할 때 마다 id값 1씩 늘려 다른id값 갖게 하기

--- todo/server.js
+++ todo/server.js
@@ -10,12 +10,6 @@ app.set('view engine' , 'ejs');
 
 let db;
 
-const obj = {
-    id:0,
-    name:'Lee',
-    age:27
-}
-
 MongoClient.connect(URL , (error , client) => {
     if(error) return console.log(error);
     db = client.db('todo');
@@ -38,15 +32,27 @@ app.get('/list' , (request , response) => {
 
 app.post('/add' , (request , response) => {
     response.send(request.body.content)
-    let obj = {
-        id:1,
-        content: request.body.content,
-        date:'2023-07-05'
-    }
-    console.log(request.body.content)
-    db.collection('post').insertOne(obj , (error , result) => {
-        console.log('저장 성공')
+
+    db.collection('counter').findOne({name: 'detailCount'} , (error , result) => {
+        let count  = result.totalPost;
+        console.log(result.totalPost);
+        console.log(request.body.content);
+
+        db.collection('post').insertOne(
+            {
+                _id: count + 1 ,
+                content :request.body.content,
+                date:'2023-07-05'
+            }, 
+            (error , result2) => {
+                error ? console.log('error발생'): console.log('저장 성공');
+
+                db.collection('counter').updateOne({name: 'detailCount'} , {$inc:{totalPost: +1}} , 
+                    () => {})
+            });
     });
+
+    
 })
 
 
